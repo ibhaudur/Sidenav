03c572071b05d2180dc8e622c9df0e548af70d17
iiicmyk <iamakilan04@gmail.com>
2022-02-24 12:52:58 +0530

    initial

---
+++ task/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Hamburger from './Component/Hamburger/Hamburger';
+
+function App() {
+  return (
+    <div className="App">
+      <Hamburger/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ task/src/Component/Hamburger/Hamburger.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./Hamburger.css";
+import { Container, Navbar } from "react-bootstrap";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Sidenav from "./Sidenav/Sidenav";
+import logo from '../Image/logo.png'
+
+export const Context = React.createContext();
+
+function Hamburger() {
+  const [view, setview] = useState(false);
+  console.log(view);
+  return (
+    <Context.Provider value={view}>
+      <Container fluid>
+        <Navbar bg="dark" expand={false}>
+          <Container className="justify-content-space-around">
+            <Navbar>
+              <img className="img" src={logo} />
+            </Navbar>
+            <h1 className="menu">
+              <GiHamburgerMenu onClick={() => setview(!view)} />
+            </h1>
+          </Container>
+        </Navbar>
+        {view ? <Sidenav /> : ""}
+      </Container>
+    </Context.Provider>
+  );
+}
+
+export default Hamburger;
